34d13756035b4e44da5831b65d40242cff7bf15d
Andy <andycv83@gmail.com>
2015-04-08 00:49:50 +0300

    2

--- src/main/java/com/home/training/dao/TaskDaoImpl.java
+++ src/main/java/com/home/training/dao/TaskDaoImpl.java
@@ -4,48 +4,43 @@ import java.util.Set;
 import main.java.com.home.training.entity.Competition;
 import main.java.com.home.training.entity.Task;
 import org.hibernate.SessionFactory;
-//import org.springframework.beans.factory.annotation...;
-//import org.springframework.stereotype.Repository;
+import org.springframework.beans.factory.annotation;
+import org.springframework.stereotype.Repository;
 
+//@Repository
 public class TaskDaoImpl implements TaskDAO {
-
-	@Override
+ //   @Autowired
+    private SessionFactory sessionFactory;
+    
 	public void addTask(Task task) {
-		// TODO Auto-generated method stub
+		sessionFactory.getCurrentSession().save(task);
 		
 	}
 
-	@Override
 	public void updateTask(Task task) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public void deleteTask(Integer id) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public Set<Task> getAllTask(int CompetitionId) {
-		// TODO Auto-generated method stub
-		return null;
+		return sessionFactory.getCurrentSession().createQuery("from Task").list();
 	}
 
-	@Override
 	public Task getTaskById(int id) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public void addCompetition(Competition competition) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public Set<Competition> getAllCompetition() {
 		// TODO Auto-generated method stub
 		return null;
